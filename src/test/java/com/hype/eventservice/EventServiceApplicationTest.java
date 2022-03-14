fafac5d979253c8b24a1a72c3d83e122f367a7f8
jupereira97 <julia.pereira@mercadolivre.com>
2022-03-14 16:06:23 -0300

    Hide database connection

--- src/test/java/com/hype/eventservice/EventServiceApplicationTest.java
+++ src/test/java/com/hype/eventservice/EventServiceApplicationTest.java
@@ -3,10 +3,10 @@ package com.hype.eventservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//class EventServiceApplicationTest {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//}
+@SpringBootTest
+class EventServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
